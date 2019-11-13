b153f636d4796920fb38a5d7b8e3dcd408a1923f
david63 <david.wood@pear-tree.org.uk>
2019-11-13 13:44:17 +0000

    Added file comparison

---
+++ adm/style/js/extservicescheck.js
@@ -0,0 +1,37 @@
+jQuery(function() {
+	$(document).ready(function()
+	{
+		$('.file_hide').hide();
+     	$('.showkey').on('click', function()
+		{
+     		var key = $(this).attr('key');
+			$('#' + key).toggle();
+
+			if ($('.files_open' + key).is(':hidden'))
+			{
+				$('.files' + key).hide();
+				$('.files_open' + key).show();
+			}
+			else
+			{
+				$('.files_open' + key).hide();
+				$('.files' + key).show();
+			}
+		});
+  	});
+
+	$("a[name=copy_pre]").click(function()
+	{
+		var id 		= $(this).attr('id');
+		var el 		= document.getElementById(id);
+		var range	= document.createRange();
+
+		range.selectNodeContents(el);
+		var sel = window.getSelection();
+		sel.removeAllRanges();
+		sel.addRange(range);
+		document.execCommand('copy');
+
+		return false;
+	});
+});
---
+++ core/yml_formatter.php
@@ -0,0 +1,106 @@
+<?php
+/**
+*
+* @package Ext Services Check Extension
+* @copyright (c) 2019 david63
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+*/
+
+namespace david63\extservicescheck\core;
+
+use phpbb\language\language;
+use david63\extservicescheck\core\functions;
+
+/**
+* yml_formatter
+*/
+class yml_formatter
+{
+	/** @var \phpbb\language\language */
+	protected $language;
+
+	/** @var \david63\extservicescheck\core\functions */
+	protected $functions;
+
+	/**
+	* Constructor for 33extcheck
+	*
+	* @param \phpbb\language\language					$language	Language object
+	* @param \david63\extservicescheck\core\functions	functions	Functions for the extension
+	*
+	* @access public
+	*/
+	public function __construct(language $language, functions $functions)
+	{
+		$this->language		= $language;
+		$this->functions	= $functions;
+	}
+
+	/**
+	* Format the .yml file for phpBB
+	*
+	* @return null
+	* @access public
+	*/
+	public function yaml_format($original_file)
+	{
+		// Add the language file
+		$this->language->add_lang('acp_extservicescheck', $this->functions->get_ext_namespace());
+
+		$handle 		= fopen($original_file, "r");
+		$formatted_file	= '';
+
+		if ($handle)
+		{
+			while (($line = fgets($handle)) !== false)
+			{
+				$length = strlen($line);
+
+				if (preg_match("/\-\ \@/", $line))
+				{
+					$formatted_file .=  substr_replace(preg_replace("/\-\ \@/", "- '@", $line), "'", $length , 0);
+				}
+				else if (preg_match("/\-\ \%/", $line))
+				{
+					$formatted_file .= substr_replace(preg_replace("/\-\ \%/", "- '%", $line), "'", $length , 0);
+				}
+				else if (preg_match("/\:\ \%/", $line))
+				{
+					$formatted_file .= substr_replace(preg_replace("/\:\ \%/", ": '%", $line), "'", $length , 0);
+				}
+				else if (preg_match("/\[\@/", $line))
+				{
+					$formatted_file .= substr_replace(preg_replace("/\[\@/", "['@", $line), "'", ($length - 2) , 0);
+				}
+				else if (preg_match("/\[\%/", $line))
+				{
+					$formatted_file .= substr_replace(preg_replace("/\[\%/", "['%", $line), "'", ($length - 2) , 0);
+				}
+				else if (strstr($line, 'pattern:'))
+				{
+					$formatted_file .= preg_replace('/pattern/', 'path', $line);
+				}
+				else if (strstr($line, 'scope: prototype'))
+				{
+					$formatted_file .= preg_replace('/scope: prototype/', 'shared: false', $line);
+				}
+				else if (strstr($line, 'scope: container'))
+				{
+					$formatted_file .= preg_replace('/scope: container/', 'shared: true', $line);
+				}
+				else if (strstr($line, 'scope: request'))
+				{
+					$formatted_file .= substr_replace($line, $this->language->lang('REQUIRES_ATTENTION'), ($length - 1), 0);
+				}
+				else
+				{
+					$formatted_file .= $line;
+				}
+			}
+			fclose($handle);
+
+			return $formatted_file;
+		}
+	}
+}
